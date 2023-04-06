e10a99ddfef7130675a3a5f3ae496b12c2c38722
Jan <102752130+k0nfus@users.noreply.github.com>
2023-04-06 11:39:01 +0200

    Math Operators

---
+++ 002 Math Operators/operators.py
@@ -0,0 +1,23 @@
+a = 4
+b = 2
+
+#Addition
+print(a + b)
+
+#Substraction
+print(a - b)
+
+#Multiplication
+print(a * b)
+
+#Division
+print(a / b)
+
+#Exponentation
+print(a ** b)
+
+#Modulo
+print(a % b)
+
+#Integer Division
+print(a//b)
