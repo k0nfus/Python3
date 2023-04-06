d13d3d58105c093448d2ba0e3e55b1e147b88218
Jan <102752130+k0nfus@users.noreply.github.com>
2023-04-06 15:25:36 +0200

    Data Types

---
+++ 003 Data Types/datatypes.py
@@ -0,0 +1,25 @@
+# Boolean
+true_or_false = False
+print("Boolean: ")
+print(true_or_false)
+
+# Integer
+a = 2
+print("Integer: ")
+print(a)
+
+# String
+b = str("hello darkness my old friend")
+print("String: " + b)
+
+# List
+c = [1, 2, 3, 4]
+d = ["a", "b", "c", "d"]
+print("List: ")
+print(c)
+print(d)
+
+# Dictionary
+e = {"first_name":"Jan", "birth_date":"16.01.1985"}
+print("Dictionary ")
+print(e)
