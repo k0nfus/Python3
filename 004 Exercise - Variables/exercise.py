88e478937a07b69365da1e6c9137fad5d5d94157
Jan <102752130+k0nfus@users.noreply.github.com>
2023-04-06 22:13:40 +0200

    Exercise

---
+++ 004 Exercise - Variables/exercise.py
@@ -0,0 +1,11 @@
+# Define a variable named city and set it equal to any string
+city = "Karlsruhe"
+
+# Define a variable named price and set it equal to any float
+price = 2.49
+
+# Define a variable named high_score and set it equal to any int
+high_score = 2
+
+# Define a variable named is_having_fun and set it to a Boolean value
+is_having_fun = True
