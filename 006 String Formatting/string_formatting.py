97a0031bc8d72a198853eff180dc6aeeccafe2e3
Jan <102752130+k0nfus@users.noreply.github.com>
2023-04-07 14:16:25 +0200

    adding other formattings

--- 006 String Formatting/string_formatting.py
+++ 006 String Formatting/string_formatting.py
@@ -1,3 +1,9 @@
 x = 10
 formatted_output = f"I have told you {x} times!"
 print(formatted_output)
+
+another_formatted_output = "Again, I have told you {} times!".format(x)
+print(another_formatted_output)
+
+yet_another_formatted_output = "Again, I have told you {} times!".format(20)
+print(yet_another_formatted_output)
