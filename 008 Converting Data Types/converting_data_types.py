ad6d8aa193970e5321af6510f6a2d8c5adea5f50
Jan <102752130+k0nfus@users.noreply.github.com>
2023-04-07 19:02:35 +0200

    commented int()

---
+++ 008 Converting Data Types/converting_data_types.py
@@ -0,0 +1,21 @@
+float = 12.9
+
+print(float)
+print(type(float))
+
+integer = int(float)
+
+# int() doesn't round up, it just cuts through
+print(integer)
+print(type(integer))
+
+string = str(integer)
+print(string)
+print(type(string))
+
+my_list = [1, 2, 3, 4]
+print(my_list)
+
+my_list_as_str = str(my_list)
+print(my_list_as_str)
+print(my_list_as_str[0])
