69636817bec009a7b1f9377df1beac1ab96d63ab
Jan <102752130+k0nfus@users.noreply.github.com>
2023-04-07 12:38:31 +0200

    String Escape Sequences

---
+++ 005 String Escape Sequences/exercise.py
@@ -0,0 +1,16 @@
+ # Set the message variable equal to any string containing a new-line escape sequence
+message = "This is\na test\nwith new lines."
+
+
+# Add a string to the mountains variable that when printed results in: /\/\/\
+# You will need to use an escape sequence more than once!
+mountains = "/\\/\\/\\"
+
+
+# Set the quotation variable to any string that contains an escaped double quotation mark
+
+quotation = "\"Test\""
+
+print(message)
+print(mountains)
+print(quotation)
---
+++ 005 String Escape Sequences/string_escape_sequence.py
@@ -0,0 +1,3 @@
+print("Dies ist ein mehrzeiliger\n\"Test\" mit \'Anführungszeichen\' und\nanderem \\Gedöns\\.")
+
+#
