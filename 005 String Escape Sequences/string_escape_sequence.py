275c041152ad29667c0baca14eeeef86f6634802
Jan <102752130+k0nfus@users.noreply.github.com>
2023-04-07 14:13:25 +0200

    changes in string_escape_sequence.py and adding String Formatting

--- 005 String Escape Sequences/string_escape_sequence.py
+++ 005 String Escape Sequences/string_escape_sequence.py
@@ -1,3 +1,1 @@
 print("Dies ist ein mehrzeiliger\n\"Test\" mit \'Anführungszeichen\' und\nanderem \\Gedöns\\.")
-
-#
---
+++ 006 String Formatting/string_formatting.py
@@ -0,0 +1,3 @@
+x = 10
+formatted_output = f"I have told you {x} times!"
+print(formatted_output)
