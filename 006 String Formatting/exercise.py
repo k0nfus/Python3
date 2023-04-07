1ddad7c8f687a99df326cb859f2e72ed7ca07220
Jan <102752130+k0nfus@users.noreply.github.com>
2023-04-07 16:07:42 +0200

    Strings and Indexes

---
+++ 006 String Formatting/exercise.py
@@ -0,0 +1,12 @@
+# Set the variable called first  to your first name.
+# Set the variable called last  to your last name.
+# Then set the variable called formatted  that interpolates both using the .format()  method.  Make sure you follow this pattern:
+# "First Name: Colt, Last Name: Steele"
+# Remember, Udemy doesn't support f-strings yet, so you have to use format()  
+
+first = "Yoan"
+last = "Tinder"
+
+formatted = "First Name: {}, Last Name: {}".format(first, last)
+
+print(formatted)
