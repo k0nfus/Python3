0186e0da9ee0147713d2afe6cec1ab880c998327
Jan <102752130+k0nfus@users.noreply.github.com>
2023-04-08 15:34:12 +0200

    while loop example

--- 014 Loops/while_loop.py
+++ 014 Loops/while_loop.py
@@ -2,3 +2,9 @@ user_response = None
 
 while user_response != "please":
     user_response = input("Ah, ah, ah, you didn't say the magic word: ")
+    
+num = 1
+
+while num < 11:
+    print(num)
+    num += 1
