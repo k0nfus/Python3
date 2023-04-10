60e5a9049c99ad6054c9e73db17b0b8b7fda3805
Jan <102752130+k0nfus@users.noreply.github.com>
2023-04-10 12:07:45 +0200

    adding dictionaries

---
+++ 017 Dictionaries/dictionary.py
@@ -0,0 +1,9 @@
+# Dictionaries are data structures that store key value pairs
+
+student = {
+    "name": "Jan",
+    "owns_cat": True,
+    "Python Skills": 0,
+    "favorite color": "maroon",
+    "favorite number": 161
+}
